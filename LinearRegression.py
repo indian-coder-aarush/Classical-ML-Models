d523c8bf8595594820998cd3bbdb06cc2609f918
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-09-17 22:08:18 +0530

    made it a bit vectorised

--- LinearRegression.py
+++ LinearRegression.py
@@ -8,18 +8,17 @@ class LinearRegression:
 
     def fit(self, X, y):
         for i in range(100):
-            for j in range(len(X)):
-                y_pred = np.dot(X[j], self.coeff) + self.intercept
-                coeff_grad = 2*(y_pred - y[j])*X[j]
-                intercept_grad = 2*(y[j] - y[j])
-                self.coeff -= 0.01*coeff_grad
-                self.intercept -= 0.01*intercept_grad
+            y_pred = X@self.coeff + self.intercept
+            coeff_grad = X.T@(2*(y_pred - y))/len(X)
+            intercept_grad = 2*(y_pred - y).sum()/len(X)
+            self.coeff -= 0.01*coeff_grad
+            self.intercept -= 0.01*intercept_grad
 
     def predict(self, X):
         return np.dot(X, self.coeff) + self.intercept
 
 X = np.array([[1,2],[3,4],[5,6],[7,8]])
-y = np.array([1,2,3,4,5])
+y = np.array([1,2,3,4])
 
 model = LinearRegression(2)
 model.fit(X,y)
