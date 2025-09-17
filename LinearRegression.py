62adc3fafb753dabfa2cebdfd3ce84c36f53ae5e
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-09-17 15:22:20 +0530

    made the fit function woth gradient descent

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,6 +3,15 @@ import pandas as pd
 
 class LinearRegression:
 
-    def __init__(self):
-        self.coeff = []
-        self.intercept = None
+    def __init__(self,size):
+        self.coeff = np.zeros(size)
+        self.intercept = np.array(0)
+
+    def fit(self, X, y):
+        for i in range(len(100)):
+            for j in range(len(X)):
+                y_pred = np.dot(X[j], self.coeff) + self.intercept
+                coeff_grad = 2*(y_pred - y[j])*self.coeff
+                intercept_grad = 2*(y[j] - self.intercept)
+                self.coeff -= 0.01*coeff_grad
+                self.intercept_grad = 0.01*intercept_grad
