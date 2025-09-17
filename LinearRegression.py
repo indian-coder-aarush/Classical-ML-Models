b2a5133ebe88395c1ca692d97395bc052605e241
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-09-17 15:29:33 +0530

    finally fixed linear regression will ad more features later

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,16 +4,16 @@ class LinearRegression:
 
     def __init__(self,size):
         self.coeff = np.zeros(size)
-        self.intercept = np.array(0)
+        self.intercept = np.array(0,dtype=float)
 
     def fit(self, X, y):
-        for i in range(len(100)):
+        for i in range(100):
             for j in range(len(X)):
                 y_pred = np.dot(X[j], self.coeff) + self.intercept
-                coeff_grad = 2*(y_pred - y[j])*self.coeff
-                intercept_grad = 2*(y[j] - self.intercept)
+                coeff_grad = 2*(y_pred - y[j])*X[j]
+                intercept_grad = 2*(y[j] - y[j])
                 self.coeff -= 0.01*coeff_grad
-                self.intercept_grad = 0.01*intercept_grad
+                self.intercept -= 0.01*intercept_grad
 
     def predict(self, X):
         return np.dot(X, self.coeff) + self.intercept
@@ -23,4 +23,5 @@ y = np.array([1,2,3,4,5])
 
 model = LinearRegression(2)
 model.fit(X,y)
-model.predict(X[0])
+for i in range(4):
+    print(model.predict(X[i]))
